e59373fe3b25579a1557a9408fbdeb29a623765f
erick-costa <erickcosta2509@gmail.com>
2024-03-08 09:14:09 -0300

    implementação controller e repository

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -11,14 +11,6 @@ public class Menu {
 
 	public static void main(String[] args) {
 		int opcao = 0;
-
-        // Teste da Classe Conta
-		Conta c1 = new Conta(3, 123, 1, "Mariana", 500000.0f);
-		c1.visualizar();
-		c1.sacar(12000.0f);
-		c1.visualizar();
-		c1.depositar(5000.0f);
-		c1.visualizar();
         
 		// Teste da Classe Conta Corrente
 		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
